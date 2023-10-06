4cee9747f1f8c2320d41d055675cbd9742deaeea
Keerthi Radhakrishnan <krad246@gmail.com>
2023-10-06 17:15:32 -0400

    Add linting, pre-commit hooks, etc. via Trunk CLI.

--- shell.nix
+++ shell.nix
@@ -1,9 +1,22 @@
 let
   pkgs = import <nixpkgs> {};
+  trunk = pkgs.callPackage ./trunk {};
 in pkgs.mkShell {
   packages = with pkgs; [
     git
     direnv
     nix-direnv
   ];
+  buildInputs = [ trunk."@trunkio/launcher-1.2.7" ];
+  shellHook = ''
+    trunk=${builtins.toString ./.trunk}
+    if [[ ! -e $trunk ]]; then
+      env --chdir=$TOP trunk init -yv
+    elif [[ -d $trunk ]]; then
+      true
+    else
+      rm -i $trunk
+      env --chdir=$TOP trunk init -yv
+    fi
+  '';
 }
---
+++ trunk/trunk.nix
@@ -0,0 +1,4 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.callPackage ./trunk-static.nix {}
+
+# TODO: Consider making the manifest also its own small meta-package.
